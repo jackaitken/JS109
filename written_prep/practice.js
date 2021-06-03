658a085357cc1b945ffffb16d476699482802f7b
Jack Aitken <jaitk11@gmail.com>
2021-06-02 20:02:14 -0400

    written prep

--- written_prep/practice.js
+++ written_prep/practice.js
@@ -1,43 +1,19 @@
-function wave(str) {
-  str = str.toLowerCase();
-  let strArr = new Array(str.length).fill(str);
-  let waveArr = [];
-
-  for (let i = 0; i < strArr.length; i++) {
-    let curWord = strArr[i];
-    curWord = curWord.split('');
-    debugger;
-    if (curWord[i] === ' ') {
-      continue;
-    } else {
-      curWord[i] = curWord[i].toUpperCase()
-      curWord = curWord.join('');
-      waveArr.push(curWord);
-    }
-  }
-
-  return waveArr;
+function solve(s){
+  let oddSubstrs = getSubstrs(s);
+  return oddSubstrs.length;
 }
 
-console.log(wave('two words'));
+function getSubstrs(s) {
+  let substrs = [];
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j <= s.length; j ++) {
+      let substr = s.slice(i, j);
+      if (Number(substr) % 2 !== 0) {
+        substrs.push(substr);
+      }
+    }
+  }
+  return substrs;
+}
 
-/*
-Given a string input we need to return an array of the same length, but 
-beginning with the first character, it should be uppercased. Then sequentially
-then next letter should be uppercased with every other letter lower cased
-
-can contain more than one word and we should bypass white spaces.
-
-
-Algorithm
-  - set a variable equal to an empty array
-  - lowercase the entire array
-  - create an array of length str.length and fill it with the str
-  - iterate over that array
-    - for each element in the array:
-      - if element[index] === '':
-        - continue
-      - else:
-        - uppercase the letter at the current index
-        - push that string to the array
-*/
+console.log(solve('65612824988293231418955126174537'));
